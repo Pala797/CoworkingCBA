18c096e7514c5f60616939801cd4d01958968300
Pala797 <106939529+Pala797@users.noreply.github.com>
2024-07-14 19:31:46 -0300

    agrege el componente footer

--- CoworkingCBA-Angular/CoworkingCBA/src/app/app.component.ts
+++ CoworkingCBA-Angular/CoworkingCBA/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { PaginaPrincipalComponent } from "./pages/pagina-principal/pagina-principal.component";
 import { NavComponent } from './pages/nav/nav.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, PaginaPrincipalComponent,NavComponent],
+  imports: [RouterOutlet, PaginaPrincipalComponent,NavComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
