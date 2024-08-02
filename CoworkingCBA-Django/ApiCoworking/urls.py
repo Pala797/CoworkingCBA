bd9f48abc45b87086401cada2a9853a56c18dca4
Pala797 <106939529+Pala797@users.noreply.github.com>
2024-08-02 02:42:55 -0300

    ultimas correcciones

--- CoworkingCBA-Django/ApiCoworking/urls.py
+++ CoworkingCBA-Django/ApiCoworking/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from ApiCoworking import views
 from .views import *
 from .views import reservar
-from .views import RegistroUsuario, LoginUsuario,obtener_reservas
+from .views import RegistroUsuario, LoginUsuario,obtener_reservas,cancelar_reserva
 
 urlpatterns = [
     path('registro', RegistroUsuario.as_view(), name='registro'), 
@@ -11,6 +11,6 @@ urlpatterns = [
     path('catalogo-salas/', views.catalogo_salas, name='catalogo_salas'),
     path('obtener_reservas/', views.obtener_reservas, name='obtener_reservas'),
     path('reservar/',views.reservar, name='reservar'),
-
+    path('cancelar_reserva/<int:reserva_id>/', views.cancelar_reserva, name='cancelar_reserva'),
     
 ]
